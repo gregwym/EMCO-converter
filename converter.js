82512033764bfc0d544530936d83347978d80dab
Greg Wang <gregwym@gmail.com>
2013-11-19 20:01:11 -0500

    Get file hash.

--- converter.js
+++ converter.js
@@ -1,11 +1,26 @@
 var debug = require('debug')('converter');
 var sqlite = require('sqlite3').verbose();
+var crypto = require('crypto');
+var fs = require('fs');
+
 var olddb = new sqlite.Database(process.argv[2], sqlite.OPEN_READONLY);
 var newdb = new sqlite.Database(process.argv[3]);
-var fileDir = process.argv[4];
+var fromDir = process.argv[4];
+var toDir = process.argv[5];
 var fileMap = {};
 
-console.log('Converting from ' + process.argv[2] + ' to ' + process.argv[3] + ', with file dir ' + process.argv[4]);
+console.log('Converting from ' + process.argv[2] + ' to ' + process.argv[3] + ', with from dir ' + fromDir + ' and to dir ' + toDir);
+
+var fileDebug = require('debug')('file');
+var insertAndCopyFile = function(path) {
+  // Get the file hash
+  var sha = crypto.createHash('sha256');
+  var file = fs.readFileSync(path);
+  var hash = sha.update(file).digest('hex');
+  fileDebug(hash + ': ' + path);
+
+
+};
 
 (function(end) {
   // Walk through all files and mapping their name to the dir.
@@ -13,14 +28,14 @@ console.log('Converting from ' + process.argv[2] + ' to ' + process.argv[3] + ',
   var walk = require('walk');
 
   debug('Start walking');
-  var walker = walk.walkSync(fileDir, null);
+  var walker = walk.walkSync(fromDir, null);
 
-  walker.on("file", function (root, fileStats, next) {
+  walker.on('file', function (root, fileStats, next) {
     // debug(root + fileStats.name);
-    fileMap[fileStats.name] = root + fileStats.name;
+    fileMap[fileStats.name] = root + '/' + fileStats.name;
   });
 
-  walker.on("end", function() {
+  walker.on('end', function() {
     debug(JSON.stringify(fileMap, null, ' '));
     debug('Finished walking');
     end();
@@ -30,6 +45,10 @@ console.log('Converting from ' + process.argv[2] + ' to ' + process.argv[3] + ',
   olddb.all('SELECT * FROM vendors', function(err, rows) {
     rows.forEach(function(row) {
       debug(JSON.stringify(row));
+      var path = fileMap[row.logo];
+      if (typeof path === 'string') {
+        var fileId = insertAndCopyFile(path);
+      }
     });
   });
 });
